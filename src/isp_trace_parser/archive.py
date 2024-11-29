2234da41ebd278819918c2db63be692011d6aaa6
Dylan McConnell <dylanjmcconnell@users.noreply.github.com>
2024-11-29 17:27:01 +1100

    Working version

--- src/isp_trace_parser/archive.py
+++ src/isp_trace_parser/archive.py
@@ -1,26 +1,57 @@
-from isp_trace_parser import solar_traces
+from pathlib import Path
+from typing import Callable, Dict
+import yaml
+from isp_trace_parser import solar_traces, wind_traces, demand_traces
 import tempfile
 import requests
 from urllib.parse import urljoin
 import zipfile
+import logging
 
-BASE_URL = "https://data.openisp.au"
+from pydantic.dataclasses import dataclass
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+OBJECT_STORE = "https://data.openisp.au"
+
+def get_trace_index():
+    url = urljoin(OBJECT_STORE, "archive/archive-index.yml")
+    response = requests.get(url)
+    return yaml.safe_load(response.text)["traces"]
+
+@dataclass
+class ArchiveTrace:
+    trace_type: str
+    version: str
+    zipfile: str
+
+    @property
+    def key(self):
+        return f"archive/traces/{self.trace_type}/{self.version}/{self.zipfile}"
+    
+    @property
+    def restructure_function(self):
+        
+        handlers: Dict[str, Callable] = {
+            "demand" : demand_traces.parse_demand_traces,
+            "solar" : solar_traces.parse_solar_traces,
+            "wind": wind_traces.parse_wind_traces
+            }
+        return handlers[self.trace_type]
 
 
-def get_archive_trace(
-    trace_type: str = "solar",
-    version: str = "isp_2024",
-    refyear: int = 2011,
-    filepath: str = "isp_wind_traces_r2011.zip",
+def get_archive(
+    archive_trace: ArchiveTrace,
+    filepath: str,
 ):
     """
     Function to get trace zipfile of particular `trace_type`, `version` and `refyear` and save to local `filepath`.
     """
 
-    url = urljoin(
-        BASE_URL,
-        f"archive/traces/{trace_type}/{version}/isp_{trace_type}_traces_r{refyear}.zip",
-    )
+    url = urljoin(OBJECT_STORE,archive_trace.key)
+    logger.info(f"Downloading {archive_trace.version} {archive_trace.zipfile}")
+
     with requests.get(url, stream=True) as response:
         with open(filepath, "wb") as file:
             for chunk in response.iter_content(chunk_size=1048576):
@@ -39,6 +70,7 @@ def unzip_trace_archive(zipfilepath: str, destination_dir: str):
 def unzip_and_restructure(
     zipfilepath: str,
     parsed_directory: str,
+    restructure_func: Callable
 ):
     """
     Function to extact a zipfile to temp directory and restructure
@@ -46,32 +78,36 @@ def unzip_and_restructure(
 
     with tempfile.TemporaryDirectory() as temp_dir:
         unzip_trace_archive(zipfilepath, temp_dir)
-        solar_traces.parse_solar_traces(temp_dir, parsed_directory)
+        restructure_func(temp_dir, parsed_directory)
 
 
-def get_unzip_and_resturcture(
-    trace_type: str = "solar",
-    version: str = "isp_2024",
-    refyear: int = 2018,
-    parsed_directory: str = "/home/dylan/Data/openISP/restructured_solar",
+def get_unzip_and_restructure(
+    archive_trace: ArchiveTrace,
+    parsed_directory: str
 ):
     """
     Function to download  zipfile to tempfile, extract to temp directory and restructure
     """
 
     with tempfile.NamedTemporaryFile() as temp_file:
-        get_archive_trace(trace_type, version, refyear, filepath=temp_file.name)
-        unzip_and_restructure(temp_file.name, parsed_directory)
+        get_archive(archive_trace=archive_trace, filepath=temp_file.name)
+        logger.info(f"Processing {archive_trace.version} {archive_trace.zipfile}")
+        unzip_and_restructure(temp_file.name, parsed_directory, archive_trace.restructure_function)
 
+def get_all(parsed_trace_data_dir: str,
+            version: str = "isp_2024"):
+    
+    store_keys = get_trace_index()
+    _dir = Path(parsed_trace_data_dir)
 
-def get_solar_project_multiple_reference_years(
-    reference_years: dict[int, int],
-    project: str,
-    directory: str,
-    year_type: str = "fy",
-):
-    """
-    Directly access restructured files from object store (not implemented etc)
-    (Idea would be to directly mimic / copy get_data functions).
-    """
-    pass
+    archive_traces = [
+    ArchiveTrace(trace_type=trace_type, version=version, zipfile=zipfile)
+    for trace_type, versions in store_keys.items()
+    for version, zipfiles in versions.items()
+    for zipfile in zipfiles
+    ]
+
+    for archive_trace in archive_traces[48:]:
+        trace_dir = _dir / archive_trace.trace_type
+        Path.mkdir(trace_dir, exist_ok=True)
+        get_unzip_and_restructure(archive_trace, parsed_directory=trace_dir)
