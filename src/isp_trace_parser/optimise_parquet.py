c4f7464b3e32bb080f4078f4c9584fcd317a81b4
Dylan McConnell <dylanjmcconnell@users.noreply.github.com>
2025-11-26 14:10:55 +1100

    Added module for optimising and paritioning parquet files

---
+++ src/isp_trace_parser/optimise_parquet.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Literal, Optional
+
+import duckdb
+from pydantic import BaseModel, validate_call
+
+
+class OptimisationConfig(BaseModel):
+    partition_cols: Optional[list[str]] = None
+    sort_by: Optional[list[str]] = None
+    delete_source: bool = True
+
+
+def partition_traces(
+    input_directory: str | Path,
+    output_directory: str | Path,
+    config: OptimisationConfig | None = None,
+):
+    if config is None:
+        config = OptimisationConfig()
+
+    output_path = Path(output_directory)
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    con = duckdb.connect()
+
+    query = f"SELECT * FROM read_parquet('{input_directory}') " "ORDER BY datetime,zone"
+
+    con.execute(f"""COPY ({query})
+                TO '{output_directory}'
+                (FORMAT PARQUET, PARTITION_BY ('reference_year'))
+                """)
+
+    con.close()
