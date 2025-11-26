e73b3093b32383176ce229c908dc7f66c5660d97
Dylan McConnell <dylanjmcconnell@users.noreply.github.com>
2025-11-26 23:30:14 +1100

    Deleted unused / old function

--- src/isp_trace_parser/optimise_parquet.py
+++ src/isp_trace_parser/optimise_parquet.py
@@ -19,34 +19,6 @@ def _delete_source_files(input_directory: str | Path) -> None:
         file.unlink()
 
 
-def partition_traces(
-    input_directory: str | Path,
-    output_directory: str | Path,
-    config: OptimisationConfig = None,
-):
-    if config is None:
-        config = OptimisationConfig()
-
-    output_path = Path(output_directory)
-    output_path.mkdir(parents=True, exist_ok=True)
-
-    con = duckdb.connect()
-
-    query = f"SELECT * FROM read_parquet('{input_directory}')"
-
-    if config.sort_by:
-        query += f" ORDER BY {', '.join(config.sort_by)}"
-
-    partition_clause = f"PARTITION_BY ({', '.join(config.partition_cols)})"
-
-    con.execute(f"""COPY ({query})
-                TO '{output_directory}'
-                (FORMAT PARQUET, {partition_clause})
-                """)
-
-    con.close()
-
-
 def partition_resource_traces(
     input_directory: str | Path,
     output_directory: str | Path,
@@ -64,8 +36,6 @@ def partition_resource_traces(
         SELECT DISTINCT reference_year
         FROM read_parquet('{input_directory}')
         """).fetchall()
-
-    partitions = reference_years
 
     for reference_year in reference_years:
         print(reference_year)
