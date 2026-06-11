283570db766816d4528aed748f769504bb8ec204
Dylan McConnell <dylanjmcconnell@users.noreply.github.com>
2026-06-11 21:15:33 +1000

    Added test for mapping_loader

---
+++ tests/test_mapping_loader.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from isp_trace_parser.mapping_loader import resource_file_metadata
+
+
+def test_resource_file_metadata():
+    """One test covers function logic compared with regex approach
+
+    Solar zones / wind zones / extra reference years add no new code-path
+    coverage (they're different YAML rows, not different code)
+    """
+    files = [
+        Path("Adelaide_Desal_FFP_RefYear2011.csv"),
+        Path("BLUFF1_RefYear2011.csv"),
+    ]
+    metadata = resource_file_metadata(files, version="2024")
+
+    assert metadata[files[0]] == {
+        "name": "Adelaide_Desal",
+        "reference_year": 2011,
+        "resource_type": "FFP",
+        "file_type": "project",
+    }
+    assert metadata[files[1]]["resource_type"] == "WIND"
